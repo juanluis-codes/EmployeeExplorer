bd29226515d5880a3f9a8aa0b706699893808d07
Juan Luis López Delgado. <juanluistenista@gmail.com>
2025-03-04 08:20:55 +0100

    Agregar archivos de proyecto.

---
+++ Employee/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeExplorer.Employee
+{
+    class Employee
+    {
+        public string EmployeeName { get; set; }
+        public int EmployeeAge { get; set; }
+        public string EmployeePosition { get; set; }
+        public DateTime EmployeeInitDate { get; set; }
+
+        public Employee(string name, int age, string position, DateTime initDate)
+        {
+            EmployeeName = name;
+            EmployeeAge = age;
+            EmployeePosition = position;
+            EmployeeInitDate = initDate;
+        }
+    }
+}
---
+++ Employee/SalariedEmployee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeExplorer.Employee
+{
+    class SalariedEmployee : Employee, IBonusable
+    {
+        public float Salary { get; set; }
+        public int ContractDurationMonths { get; set; }
+        public SalariedEmployee(string name, int age, string position, DateTime initDate) : base(name, age, position, initDate)
+        {
+        }
+
+        public int CalculateBonus(int objectivesAchieved, float pricePerObjective)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
